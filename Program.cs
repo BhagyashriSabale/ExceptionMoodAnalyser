14089f7aadbb6fec33c2bcf64a1188c4544ba524
BhagyashriSabale <balisabale16@gmail.com>
2023-05-21 16:18:11 +0530

    [Bhagyashri] Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionMoodAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Exception Mood Analyser Problem");
+        }
+    }
+}
